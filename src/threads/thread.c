bb022a1ea25043cf705875e6ba883da1b62f092e
clundro <859287553@qq.com>
2023-03-31 21:54:58 +0800

    [p1] lab2 priority (pirority insert) finish.
    Signed-off-by: clundro <859287553@qq.com>

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -70,6 +70,9 @@ static void *alloc_frame (struct thread *, size_t size);
 static void schedule (void);
 void thread_schedule_tail (struct thread *prev);
 static tid_t allocate_tid (void);
+static bool thread_priority_greater (const struct list_elem *a,
+                                     const struct list_elem *b,
+                                     void *aux UNUSED);
 
 /** Initializes the threading system by transforming the code
    that's currently running into a thread.  This can't work in
@@ -237,7 +240,8 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+  list_insert_ordered (&ready_list, &t->elem,
+                       (list_less_func *)&thread_priority_greater, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
 }
@@ -308,7 +312,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread)
-    list_push_back (&ready_list, &cur->elem);
+    list_insert_ordered (&ready_list, &cur->elem,
+                         (list_less_func *)&thread_priority_greater, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -577,6 +582,14 @@ allocate_tid (void)
   lock_release (&tid_lock);
 
   return tid;
+}
+
+static bool
+thread_priority_greater (const struct list_elem *a, const struct list_elem *b,
+                         void *aux UNUSED)
+{
+  return list_entry (a, struct thread, elem)->priority
+         > list_entry (b, struct thread, elem)->priority;
 }
 
 /** Offset of `stack' member within `struct thread'.
